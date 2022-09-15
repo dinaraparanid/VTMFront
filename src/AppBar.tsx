84ddd6d75779c07b4591a68dd058b8afa2ebacce
paranid5 <dinaraparanid@gmail.com>
2022-09-15 23:17:07 +0300

    localization start

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/AppBar.css'
 import './styles/Lang.css'
+import {Localisation, toLanguage} from "./utils/Localisation";
 
 const Title = () => (<a href='home' className="Title"><h2>Video To Music Converter</h2></a>)
 
@@ -11,17 +12,19 @@ const Menu = ({languages}: {languages: Array<String>}) => (
         <div className='Dropdown'>
             <button className='LangButton'>Language</button>
             <div className='DropdownContent'>
-                { languages.map(lang => (<a href='#' style={{color: '#ffab00'}}>{lang}</a>)) }
+                { languages.map((lang, index) => (<button
+                    onClick={ () => Localisation.getInstance().setLang(toLanguage(index)) }
+                    style={{color: '#ffab00'}}>{lang}</button>)) }
             </div>
         </div>
-        <a href='about' className='Column'>About</a>
+        <a href='about' className='Column'>About App</a>
     </div>
 )
 
 const AppBar = () => (
     <div className='AppBar'>
         <Title/>
-        <Menu languages={['English', 'Русский']}/>
+        <Menu languages={['Русский', 'English']}/>
     </div>
 )
 
--- src/Converter.tsx
+++ src/Converter.tsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import './styles/Converter.css'
 
-const Converter = () => (
-    <div className='Converter'>
-        <p className='PasteUrl'>Paste URL link here</p>
-        <div className='Row'>
-            <input className='Input' type='text' placeholder='Your URL link video'/>
-            <button className='DownloadButton'>Download</button>
-        </div>
-        <div className='Formats'>
-            <button className='MP3Format'>MP3</button>
-            { ['MP4', 'WAV', 'AAC'].map(format => (<button className='Format'>{format}</button>)) }
-        </div>
-    </div>
-)
+enum OutputFormat { MP3 = 'MP3', MP4 = 'MP4', WAV = 'WAV', AAC = 'AAC' }
 
-export default Converter;
+export default function Converter() {
+    const outputFormat = useState(OutputFormat.MP3)
+
+    return (
+        <div className='Converter'>
+            <p className='PasteUrl'>Paste URL link here</p>
+            <div className='Row'>
+                <input className='Input' type='text' placeholder='Your URL link'/>
+                <button
+                    className='DownloadButton'
+                    onClick={() => console.log('TODO: Started downloading') }
+                >Download</button>
+            </div>
+            <div className='Formats'>
+                <button
+                    className='MP3Format'
+                    onClick={() => outputFormat[0] = OutputFormat.MP3}
+                >MP3</button> {
+                [OutputFormat.MP4, OutputFormat.WAV, OutputFormat.AAC].map(format =>
+                    (<button
+                        className='Format'
+                        onClick={() => outputFormat[0] = format}
+                    >{format}</button>))
+            }</div>
+        </div>
+    )
+}
